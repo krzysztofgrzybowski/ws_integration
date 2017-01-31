ea481c81917cbaaa363d0137967b7e33cb67a65e
krzysztofgrzybowski <krzysztof.grzybowski@polcode.net>
2017-01-31 12:58:26 +0100

    Add migration for worksnaps_user

---
+++ lib/generators/ws_integration/ws_integration_generator.rb
@@ -0,0 +1,13 @@
+module WsIntegration
+  class WsIntegrationGenerator < Rails::Generators::Base
+    desc "This generator creates worksnaps_user migration."
+
+    source_root File.expand_path("../templates", __FILE__)
+
+    argument :class_name, type: :string, default: "user"
+
+    def cerate_model
+      generate "migration", "create_worksnaps_user worksnaps_id:integer #{class_name.downcase}:references"
+    end
+  end
+end
