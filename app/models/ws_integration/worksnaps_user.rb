15250e1dec736a3b46b2d1e78c56333fef69636f
krzysztofgrzybowski <krzysztof.grzybowski@polcode.net>
2017-02-01 13:32:42 +0100

    Add synchronize function

--- app/models/ws_integration/worksnaps_user.rb
+++ app/models/ws_integration/worksnaps_user.rb
@@ -1,11 +1,33 @@
 module WsIntegration
   class WorksnapsUser < ActiveRecord::Base
     def self.synchronize
-      puts '**********************************************'
-      puts 'TOKEN: ' + Rails.configuration.x.worksnaps_token
-      puts HTTP.basic_auth(user: Rails.configuration.x.worksnaps_token, pass: '')
-                .get("https://api.worksnaps.com/api/users.xml").body
-      puts '**********************************************'
+      users_synchronized = 0
+      api_response = HTTP.basic_auth(user: Rails.configuration.x.worksnaps_token, pass: '')
+                         .get("https://api.worksnaps.com/api/users.xml").body
+      Nokogiri::XML(api_response).xpath("//user").each do |ws_user|
+        # index: 0 - ws_id; 4 - user_email
+        ws_user = ws_user.text.split
+        application_user = Rails.configuration.x.user_class_name.constantize
+          .find_by(email: ws_user[4])
+        if application_user
+          users_synchronized += 1
+          WorksnapsUser.find_or_create_by(
+            worksnaps_id: ws_user[0],
+            "#{Rails.configuration.x.user_class_name.downcase}_id": application_user.id
+          )
+        end
+      end
+      users_synchronized
+    end
+
+    def self.get_worksnaps_data(user)
+      ws_user = WorksnapsUser.find_by(user_id: user.id)
+      if ws_user
+        api_response = HTTP.basic_auth(user: Rails.configuration.x.worksnaps_token, pass: '')
+                           .get("https://api.worksnaps.com/api/users/#{ws_user.worksnaps_id}.xml").body
+        api_response = Nokogiri::XML(api_response).xpath("//user").text.split
+        puts api_response
+      end
     end
   end
 end
--- lib/generators/ws_integration/ws_integration_generator.rb
+++ lib/generators/ws_integration/ws_integration_generator.rb
@@ -5,15 +5,14 @@ module WsIntegration
     source_root File.expand_path("../templates", __FILE__)
 
     argument :class_name, type: :string, default: "User"
-    argument :token, type: :string, default: "YOUR_WORKSNAPS_TOKEN"
 
     def create_migration
-      generate "model", "create_worksnaps_user worksnaps_id:integer #{class_name.downcase}:references --no-assets --no-test-framework"
+      generate "migration", "create_ws_integration_worksnaps_user worksnaps_id:integer #{class_name.downcase}:references --no-assets --no-test-framework"
     end
 
     def add_config
       create_file "config/initializers/ws_integration.rb" do
-        "Rails.configuration.x.user_class_name = '#{class_name}'\nRails.configuration.x.worksnaps_token = '#{token}'"
+       "Rails.configuration.x.user_class_name = '#{class_name}'\nRails.configuration.x.worksnaps_token = 'YOUR_WORKSNAPS_TOKEN'"
       end
     end
   end
